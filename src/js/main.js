c3a40c65fccb2ef02c8cc17842030c7c79de01a3
Lucas Menendez <epucas@gmail.com>
2020-10-29 19:02:10 +0100

    portfolio structure section

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,30 @@
 import "../styles/main.scss";
 
+
 ScrollOut({
     cssProps: {
         viewportY: true,
         visibleY: true,
     }
 });
+
+
+function getMedia() {
+    return new Promise((resolve, reject) => {
+        new InstagramFeed({
+            username: "adrimaxtattoo",
+            callback: data => {
+                const media = data.edge_owner_to_timeline_media || null
+                if (media && media.edges) resolve(media.edges.map(img => {
+                    return {
+                        picture: img.node.display_url,
+                        thumbnail: img.node.thumbnail_src
+                    }
+                }));
+            },
+            on_error: err => reject(err)
+        });
+    });  
+}
+
+//getMedia().then(data => console.log(data));
